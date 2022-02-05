fe0142a9c6ce8fd5eb1db48e07d9d9edded32be9
sarahangelou <55256710+sarahangelou@users.noreply.github.com>
2022-02-05 16:02:35 -0500

    firebase connected

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import * as firebase from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +22,6 @@ let app = initializeApp(firebaseConfig);
     app = firebase.app();
 }*/
 
-const db = getFirestore(app);
+const db = firebase.getFirestore(app);//firebase;
 
-export { db }
+export { app, db }
--- hooks/getRecipes.ts
+++ hooks/getRecipes.ts
@@ -1,27 +1,14 @@
 import { doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from '../firebase';
 import { collection, setDoc } from "firebase/firestore"; 
+import { useEffect, useState } from "react";
 
-export async function getData (){
+export async function getRecipe (id: string){
+  
 
-    console.log("in get data")
-    //const recipesCol = collection(db, 'recipes');
-    const docRef = doc(db, "recipes", "p3pKOD6jIHEcjf20CCXohP8uqkG5dGi");
-    const docSnap = await getDoc(docRef);
-    //.then((response) =>{
-    //    console.log(response);
-    //});
+}
 
-    //const cityList = docSnap.docs.map(doc => console.log(doc.data()));
+export async function getAllRecipes (){
 
-
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      return docSnap.data();
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-      return "";
-    }
 
   }
