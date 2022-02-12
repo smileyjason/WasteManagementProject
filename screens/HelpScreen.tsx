8b456024dff824ff751745e324fe88624e9ed8ce
pun49030 <73437143+pun49030@users.noreply.github.com>
2022-02-12 11:19:36 -0500

    Updated helpscreen

--- screens/HelpScreen.tsx
+++ screens/HelpScreen.tsx
@@ -6,9 +6,18 @@ import { RootTabScreenProps } from '../types';
 
 export default function HelpScreen({ navigation }: RootTabScreenProps<'HelpScreen'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>HALP</Text>
+    <View style={styles.container_left}>
+      <Text style={styles.title}>HELP</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.description}>Learn about the app's features and our mission</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.description}>#Video walkthrough of the app</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.left_title}>Our Mission</Text>
+      <Text style={styles.description}>Text on our mission</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.left_title}>Our Values</Text>
+      <Text style={styles.description}>Text on our values</Text>
     </View>
   );
 }
